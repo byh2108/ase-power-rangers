45ece9e85ccc06d72f8d38ea0490e5d3fcf05641
applekuo92 <applekuo92@gmail.com>
2016-12-09 21:09:24 -0500

    coverage report ....

--- toy_system/choose/tests.py
+++ toy_system/choose/tests.py
@@ -4,7 +4,7 @@ from choose.models import *
 from django.core.urlresolvers import reverse
 from decimal import Decimal
 
-def create_new_dish(name,cost):
+def create_new_dish(name,cost,description="",categories='Appetizer'):
     new_dish = Menu(name=name,cost=cost)
     new_dish.save()
     return new_dish
@@ -17,7 +17,7 @@ def create_new_record(name):
 class TotalPriceMethodTests(TestCase):
     def test_price_with_zero_item(self):
         new_record = create_new_record('test')
-        self.assertIs(new_record.total_price(), 0)
+        self.assertIs(new_record.total_price(), 0.0)
 
     def test_price_with_one_items_one_quantity(self):
     	new_record = create_new_record('test')
@@ -97,3 +97,103 @@ class TotalPriceMethodTests(TestCase):
         new_record.cust_order.create(dish_id = new_dish2,quantity=2.00)
         self.assertEqual(new_record.total_price(), 300)
 
+class MenuTests(TestCase):
+    def test_menu_with_one_dish(self):
+        new_dish = create_new_dish(name = "apple pie", cost = 1.32)
+        self.assertQuerysetEqual(
+            Menu.objects.all(),
+            ['<Menu: apple pie>']
+        )
+        self.assertEqual(str(new_dish), "apple pie")
+        self.assertEqual(new_dish.categories, 'Appetizer')
+
+    def test_menu_with_zero_dish(self):
+        self.assertQuerysetEqual(
+            Menu.objects.all(),
+            []
+        )
+
+class CustRecordTests(TestCase):
+    def test_custrecord_with_one(self):
+        new_record = create_new_record('test')
+        self.assertQuerysetEqual(
+            CustRecord.objects.all(),
+            ['<CustRecord: test>']
+        )
+
+    def test_custrecord_with_zero(self):
+        self.assertQuerysetEqual(
+            CustRecord.objects.all(),
+            []
+        )
+
+class OrderRecordTests(TestCase):
+    def test_orderrecord_with_one(self):
+        new_record = create_new_record('test')
+        new_dish = create_new_dish('Lobster',100)
+        new_order_record = OrderRecord(cust_id = new_record,dish_id = new_dish, quantity = 1)
+        new_order_record.save()
+        now = str(new_order_record.order_date)
+        x = "Order Date: " + now + " ---- Custmer Name:  " + new_order_record.cust_id.name
+        self.assertQuerysetEqual(
+            OrderRecord.objects.all(),
+            ['<OrderRecord: ' + x + '>']
+        )
+
+class ReviewTest(TestCase):
+    def test_review_with_one(self):
+        new_record = create_new_record('test')
+        new_dish = create_new_dish('Lobster',100)
+        new_review = Review(cust_id = new_record, dish_id = new_dish,context = 'good!!')
+        new_review.save()
+        now = str(new_review.create_date)
+        x = "Review Date: " + now + " ---- Custmer Name:  " + new_review.cust_id.name
+        self.assertQuerysetEqual(
+            Review.objects.all(),
+            ['<Review: ' + x + '>']
+        )
+
+class MenuVideoTest(TestCase):
+    def test_video_with_one(self):
+        new_dish = create_new_dish('Lobster',100)
+        new_video = MenuVideo(name = "new video", video = "https://aaa.abc.com", menu_video = new_dish)
+        new_video.save()
+        self.assertQuerysetEqual(
+            MenuVideo.objects.all(),
+            ['<MenuVideo: new video>']
+        )
+    def test_video_with_zero(self):
+        self.assertQuerysetEqual(
+            MenuVideo.objects.all(),
+            []
+        )
+
+class MenuViewTests(TestCase):
+    def test_index_view_with_one_dish(self):
+        new_dish = create_new_dish(name = "apple pie", cost = 1.32,description="good",categories='Appetizer')
+        response = self.client.get(reverse('index'))
+        self.assertQuerysetEqual(
+            response.context['list_of_pie'],
+            ['<Menu: apple pie>']
+        )
+
+    def test_index_view_with_zero_dish(self):
+        response = self.client.get(reverse('index'))
+        self.assertQuerysetEqual(
+            response.context['list_of_pie'],
+            []
+        )
+
+class ResultViewTests(TestCase):
+    def test_result_view_cust_exist(self):
+        new_record = create_new_record('test')
+        response = self.client.get(reverse('result',args=(new_record.id,)))
+        self.assertQuerysetEqual(
+            response.context['cust'],
+            ['<CustRecord: test>']
+        )
+
+    def test_result_view_cust_not_exist(self):
+        cid = 1
+        response = self.client.get(reverse('result',args=(cid,)))
+        self.assertEqual(response.status_code,404)
--- toy_system/toy_system/settings.py
+++ toy_system/toy_system/settings.py
@@ -16,6 +16,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
 
+
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '5^69q9-78o-)wvj*l9zc9anhi&n#)s6mhnqin$+4n*e%k7$vg-'
 
@@ -44,6 +45,7 @@ TEMPLATES = [
 # Application definition
 
 INSTALLED_APPS = (
+    'django_nose',
     'choose.apps.ChooseConfig',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -110,3 +112,14 @@ STATIC_URL = '/static/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media/')
+
+
+
+# Use nose to run all tests
+TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
+
+# Tell nose to measure coverage on the 'foo' and 'bar' apps
+NOSE_ARGS = [
+    '--with-coverage',
+    '--cover-package=choose',
+]
